243a52e52ae3268077f43b27ad2affb01bef702d
Destux <pandeysv@rknec.edu>
2022-06-12 17:00:42 +0530

    Some changes

--- website-frontend/src/App.js
+++ website-frontend/src/App.js
@@ -1,59 +1,17 @@
-// import './App.css';
-<<<<<<< HEAD
 import Article from "./routes/js/Article";
-import Newevent1 from './routes/js/Newevent1';
 import Event_App from './routes/js/Event_app';
 import Registration from "./routes/js/Registration";
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
-=======
-// import Newevent1 from './routes/js/Newevent1';
-// import Article from "./routes/js/Article";
-// import Newevent1 from './routes/js/Newevent1';
-// import Event_App from './routes/js/Event_app';
-// import Registration from "./routes/js/Registration";
-// import { BrowserRouter, Route, Routes} from 'react-router-dom';
-<<<<<<< HEAD
- import Home from './routes/js/Home';
-=======
->>>>>>> 26ac93524b565ed3a399a8fe3afa548b74f9f03c
+import Newevent1 from './routes/js/Newevent1';
 import Home from './routes/js/Home';
->>>>>>> 0334f5564b0c47f9d9060617d746a2bbabadb331
-// import Register from './routes/js/Register';
 import Login from './routes/js/Login';
-import Navigation from './routes/js/Navigation';
-// import Event from './routes/Event';
 import Achievements from "./routes/js/Achievements";
 import About_us from "./routes/js/About_us";
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-    <Registration></Registration>
-    {/* <About_us/> */}
-    {/* <Login/> */}
-    {/* <Achievements/> */}
-    {/* <About_us/>
-    <Home />
-    <Newevent1/> 
-     <Registration>
-    <Navigation /> */}
-    {/* <Event_App/> */}
-    {/* <Newevent1 /> */}
-  {/* <Navigation /> */}
-      {/* <Routes>
-=======
-<<<<<<< HEAD
-    {/* <Achievements></Achievements> */}
-    <Home />
-    {/* <Newevent1/> */}
-    {/* <Registration/> */}
-    {/* <Navigation />
-=======
->>>>>>> 0334f5564b0c47f9d9060617d746a2bbabadb331
-    
       <Routes>
->>>>>>> 26ac93524b565ed3a399a8fe3afa548b74f9f03c
           <Route index element={<Home />} />
           <Route path="login" element={ <Login />} />
           <Route path="achievements" element={ <Achievements />} />
@@ -62,7 +20,7 @@ function App() {
           <Route path="registration" element={ <Registration />} />
           <Route path="about_us" element={ <About_us />} />
           <Route path="main_event" element={ <Newevent1 />} />
-  </Routes>  */}
+        </Routes> 
     </>
   );
 }
